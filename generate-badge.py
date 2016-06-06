3be834c56c37db7e52abf790a3b196fce034e61e
Owen Garland <owen.garland@clock.co.uk>
2016-06-06 16:36:58 +0100

    Added basic flask server

--- generate-badge.py
+++ generate-badge.py
@@ -1,8 +1,32 @@
 import svgwrite
+from flask import Flask
 
-dwg = svgwrite.Drawing('test.svg', profile='tiny')
-dwg.add(dwg.rect((0, 0), (87, 20), fill='rgb(85,85,85)'))
-dwg.add(dwg.text('build', insert=(6, 14), fill='rgb(255,255,255)', font_family = 'DejaVu Sans,Verdana,Geneva,sans-serif', font_size = '11px'))
-dwg.add(dwg.rect((37, 0), (51, 20), fill='rgb(68,204,17)'))
-dwg.add(dwg.text('passing', insert=(40.5, 14), fill='rgb(255,255,255)', font_family = 'DejaVu Sans,Verdana,Geneva,sans-serif', font_size = '11px'))
-dwg.save()
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Enter your API URL here: '
+
+@app.route('/badge')
+def get_image():
+    return generateBadge(False, 'build', '555555', 'passing', '44cc11')
+
+def generateBadge(apiURL, labelText, labelColour, valueText, valueColour):
+    dwg = svgwrite.Drawing(profile='tiny')
+
+    dwg.add(dwg.rect((0, 0), (87, 20), fill=hex_to_rgb(labelColour)))
+    dwg.add(dwg.text(labelText, insert=(6, 14), fill='rgb(255,255,255)', font_family = 'DejaVu Sans,Verdana,Geneva,sans-serif', font_size = '11px'))
+
+    dwg.add(dwg.rect((37, 0), (51, 20), fill=hex_to_rgb(valueColour)))
+    dwg.add(dwg.text(valueText, insert=(40.5, 14), fill='rgb(255,255,255)', font_family = 'DejaVu Sans,Verdana,Geneva,sans-serif', font_size = '11px'))
+
+    return dwg.tostring()
+
+def hex_to_rgb(value):
+    value = value.lstrip('#')
+    def get_int(pos):
+        start = pos * 2
+        return int(value[start:start+2], 16)
+    return 'rgb(' +  str(get_int(0)) + ',' + str(get_int(1)) + ',' + str(get_int(2)) + ')'
+
+app.run(debug = True)
